a8cd8427c36ebc610773c39e66e1830f49113510
David DIDIER <c_inconnu1@yahoo.fr>
2012-10-24 21:03:58 +0200

    Added be_sorted_by and be_reverse_sorted_by matchers.

--- lib/ndd/rspec/matchers.rb
+++ lib/ndd/rspec/matchers.rb
@@ -1,12 +1,17 @@
 # encoding: utf-8
-require_relative 'matchers/test-matcher'
+require_relative 'matchers/be_reverse_sorted_by'
+require_relative 'matchers/be_sorted_by'
 
 module Ndd
   module RSpec
     module Matchers
 
-      def be_equal_ndd(expected)
-        BeEqualNdd.new(expected)
+      def be_reverse_sorted_by(attribute)
+        BeReverseSortedBy.new(attribute)
+      end
+
+      def be_sorted_by(attribute)
+        BeSortedBy.new(attribute)
       end
 
     end
---
+++ lib/ndd/rspec/matchers/be_sorted_by.rb
@@ -0,0 +1,48 @@
+# encoding: utf-8
+require 'rspec/expectations'
+require 'rspec/expectations/differ'
+
+
+module Ndd
+  module RSpec
+    module Matchers
+
+      # ----------------------------------------------------------------------------------------------------------------
+      # Ensures that an enumerable (responding to <code>Enumerable#collect</code>) is sorted by the given attribute.
+      #
+      # Examples:
+      #   [MyObject.new(my_attribute: 1), MyObject.new(my_attribute: 2)].should be_sorted_by(:my_attribute) }
+      #
+      class BeSortedBy
+
+        def initialize(attribute = nil)
+          @attribute = attribute
+        end
+
+        def matches?(actual)
+          @actual = actual
+          attributes = actual.collect { |e| e.send(@attribute) }
+          @actual_attributes = attributes
+          @sorted_attributes = attributes.sort
+          @sorted_attributes == @actual_attributes
+        end
+
+        def failure_message_for_should
+          return <<-MESSAGE
+
+expected '#{@actual.inspect}' to be sorted by '#@attribute'
+expected attributes: #{@sorted_attributes.inspect}
+     got attributes: #{@actual_attributes.inspect}
+
+          MESSAGE
+        end
+
+        def ==(other)
+          matches?(other)
+        end
+
+      end
+
+    end
+  end
+end
